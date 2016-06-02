d9eba46e6735ac86d78f9eed82c113538230cafd
stevielam <slam121212@yahoo.com>
2016-06-02 10:51:01 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ TIMEZONE="America/Los_Angeles"
 
 
 echo 'Choose Password for MySQL root and PHPMyAdmin root: ' 
-sudo read -s -p PASSWORD1
+read -s -p PASSWORD1
 
 echo 'Please repeat the password: ' 
-sudo read -s -p PASSWORD2
+read -s -p PASSWORD2
 
 # Check both passwords match
 if [ $PASSWORD1 != $PASSWORD2 ]; then
