cb66161657beffba913570300a583f07bcad5f0f
stevielam <slam121212@yahoo.com>
2016-06-02 06:52:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,9 +25,10 @@ sudo apt-get install -y git-core || (echo "Git Install Failed. Aborting..." && e
 	#cd ~
 #fi	
 
-if [ -d ~/wiringPi ] then 
-	#sudo echo "wiringPi exists. Deleting..."
-	sudo rm -R -f ~/wiringPi || (echo "WiringPi Delete Failed. Aborting..." && exit 1) 
+if [ -d ~/wiringPi ] 
+then 
+	sudo echo "wiringPi exists. Deleting..."
+	sudo rm -rf wiringPi || (echo "WiringPi Delete Failed. Aborting..." && exit 1) 
 fi
 
 git clone git://git.drogon.net/wiringPi || (echo "WiringPi Clone Failed. Aborting..." && exit 1) 
