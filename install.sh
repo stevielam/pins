f554dbaeda61266ed9a41fd4c0091547b2993a2a
Steve Lam <slam@paramountfarms.com>
2016-06-01 20:00:34 -0700

    sdsd

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ PASSWORD=test
 (sudo apt-get update && sudo apt-get -y upgrade) || (echo "Upgrade Failed. Aborting..." && exit 1)  
 
 #set the timezone
-echo "America/Los_Angeles" > /etc/timezone
+echo "America/Los_Angeles" > sudo /etc/timezone
 
 #install git
 sudo apt-get install -y git-core || (echo "Git Install Failed. Aborting..." && exit 1)
@@ -31,16 +31,16 @@ sudo chmod g+s -R /var/www/ || (echo "Setting Permissions Failed. Aborting..." &
 sudo usermod -a -G www-data pi || (echo "Adding pi to www-data Failed. Aborting..." && exit 1)
 
 #installing mysql
-sudo echo 'mysql-server mysql-server/root_password password $PASSWORD' | debconf-set-selections
-sudo echo 'mysql-server mysql-server/root_password_again password $PASSWORD' | debconf-set-selections
+sudo echo 'mysql-server mysql-server/root_password password $PASSWORD' | sudo debconf-set-selections
+sudo echo 'mysql-server mysql-server/root_password_again password $PASSWORD' | sudo debconf-set-selections
 sudo apt-get install -y mysql-server mysql-client || (echo "MySQL Install Failed. Aborting..." && exit 1)
 
 #install phpmyadmin
-sudo echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
-sudo echo "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD" | debconf-set-selections
-sudo echo "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD" | debconf-set-selections
-sudo echo "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD" | debconf-set-selections
-sudo echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+sudo echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | sudo debconf-set-selections
+sudo echo "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD" | sudo debconf-set-selections
+sudo echo "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD" | sudo debconf-set-selections
+sudo echo "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD" | sudo debconf-set-selections
+sudo echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo debconf-set-selections
 sudo apt-get install -y phpmyadmin || (echo "PHPMyAdmin Install Failed. Aborting..." && exit 1)
 
 #create database and tables
